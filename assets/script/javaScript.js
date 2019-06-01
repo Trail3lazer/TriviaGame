0e7c48bfcf69085d7a7269dec5b996c1436a6f3d
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-01 14:53:03 -0600

    created some questions

---
+++ assets/script/javaScript.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+        question: "What state was Jared born in?",
+        answers: ["Utah", "Washington", "Virginia", "Idaho"],
+        Correct: 0,
+    },
+    
+    {
+        question: "How many siblings does Jared have",
+        answers: ["Two", "Three", "Four", "Five"],
+        Correct: 1,
+    },
+
+    {
+        question: "What mountain has Jared summited?",
+        answers: ["Timpanogos", "Ranier", "Hood", "Baker"],
+        Correct: 3,
+    },
+
+    {
+        question: "What year did Jared graduate Highschool?",
+        answers: ["2010", "2012", "2014", "2015"],
+        Correct: 2,
+    },
+
+    {
+        question: "How old was Jared when he built his first computer?",
+        answers: ["12", "13", "14", "15"],
+        Correct: 0,
+    },
+
+    {
+        question: "What has Jared built from yard junk?",
+        answers: ["Go-Kart", "Mountain Board", "Trebuche", "Hover Craft"],
+        Correct: 3,
+    },
+
+    {
+        question: "What's Jared's favorite kicks?",
+        answers: ["Roundhouse", "Kyrie 3", '"Kick"-starter.com', "Recoil"],
+        Correct: 1,
+    },
+];
+
+let questionCounter = 0;
+let answerVeracity = Boolean
+
+// function to change to the next question and answer set
+// randomize answers
+
+// timeout function
+// time remaining notifier
+// correct/incorrect record keeper
+// t/f checker
+// correct notification
+// incorrect notification
+// restart fx
