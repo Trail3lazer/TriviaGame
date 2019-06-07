923efefa83127a7f96736dde5ceb1e8c816bff32
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-06-06 20:02:41 -0600

    Fixed up the timer logic and organized things

--- assets/script/javaScript.js
+++ assets/script/javaScript.js
@@ -1,4 +1,4 @@
-let questionsIndex = 0
+let questionsIndex = -1
 let questions = [
     {
         question: "What state was Jared born in?",
@@ -42,45 +42,116 @@ let questions = [
         Correct: "Kyrie 3",
     },
 ];
-let answerVeracity = Boolean;
-let timer;
+let seconds = 5;
+let count;
 let correct = 0;
 let incorrect = 0;
 
+// time remaining notifier
+// & timeout function
+
+function atInterval() {
+    seconds = seconds-1;
+    $("#timerId").html(seconds);
+    if (seconds === 0) {
+        clearInterval(count);
+        printNextQuestion();
+    }
+};
+
+function countdown() {
+    seconds = 5;
+    clearInterval(count);
+    count = setInterval(atInterval, 1000);
+};
+
+function ending() {
+    if (questionsIndex === questions.length) {
+        if (correct>incorrect+1) {
+        $("#timerBanner").hide()
+        $("#topRow").append("<h1 class='container bg-success rounded' id='announcement'> You know Jared pretty well!</h1>")
+        $("#questionId").html("Correct: "+correct+"  <br><br>"+"Incorrect: "+incorrect+"   <br><br>")
+        $("#answersID-0").hide()
+        $("#answersID-1").hide()
+        $("#answersID-2").hide()
+        $("#answersID-3").hide()
+        $("#answers").append('<div class="btn btn-danger btn-lg" id="reset"> RESET </div>')
+        restart()
+        } else {
+
+            // $("#questionId").html("Maybe you should ask Jared some questions and try again...!")
+            // $("#answersID-0").html("Correct: "+correct)
+            // $("#answersID-1").html("Incorrect: "+incorrect)
+            // $("#answersID-2").html('')
+            // $("#answersID-3").html('<div class="btn btn-danger" id="reset"> RESET </div>')
+
+        $("#timerBanner").hide()
+        $("#topRow").append("<h1 class='container bg-danger rounded' id='announcement'> Maybe you should ask Jared some questions and try again...! </h1>")
+        $("#questionId").html("Correct: "+correct+"  <br><br>"+"Incorrect: "+incorrect+"   <br><br>")
+        $("#answersID-0").hide()
+        $("#answersID-1").hide()
+        $("#answersID-2").hide()
+        $("#answersID-3").hide()
+        $("#answers").append('<div class="btn btn-danger btn-lg" id="reset"> RESET </div>')
+        restart()
+        }
+    };
+}
+
 // function to change to the next question and answer set
-let printNextQuestion = function() {
+function printNextQuestion() {
+    questionsIndex = questionsIndex + 1;
+    if (questionsIndex < questions.length){
     $("#questionId").html(questions[questionsIndex].question)
     for (var i = 0; i < questions[questionsIndex].answers.length; i++) {
         $("#answersID-"+i).html(questions[questionsIndex].answers[i])
     };
-    //timer = setTimeout(truthChecker, 5000)
-}
+    countdown();
+    }
+    ending();
+};
 
-// randomize answers
-
-// timeout function
-
-// time remaining notifier
 // correct/incorrect record keeper
 // t/f checker
-$(".btn-primary").click(function() {
-    clearTimeout(timer)
-    if ($(this).text() === questions[questionsIndex].Correct) {
+function truthChecker(btn) {
+    clearInterval(count)
+    if (questionsIndex < questions.length){
+    if ($(btn).text() === questions[questionsIndex].Correct) {
         alert("Hooray!!!");
         correct++
-        questionsIndex = questionsIndex + 1
-        printNextQuestion();
+
      } else {
        alert("Nope");
-       incorrect++
-       questionsIndex = questionsIndex + 1
-       printNextQuestion();
-     }
+       incorrect++;
+     };
+     printNextQuestion();
+    }
+
+};
+
+$(".btn-primary").click(function() {
+    truthChecker(this)
 });
 
-
-// correct notification
-// incorrect notification
 // restart fx
+function restart() {
+    $(".btn-danger").click(function() {
+        console.log('working!!!!')
+        questionsIndex = -1;
+        seconds = 5;
+        correct = 0;
+        incorrect = 0;
 
-printNextQuestion();
+        $("#timerBanner").show()
+        $("#announcement").remove()
+        $("#answersID-0").show()
+        $("#answersID-1").show()
+        $("#answersID-2").show()
+        $("#answersID-3").show()
+        $("#reset").remove()
+        printNextQuestion()
+    })
+}
+
+
+printNextQuestion()
